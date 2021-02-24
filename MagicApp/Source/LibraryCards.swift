3005dc8a19ffee59afec27ca396a55a959dd17f4
Felipe Henriques <felipeferreirahenriques@outlook.com>
2021-02-24 17:47:13 -0300

    Barra de pesquisa funcionando

--- MagicApp/Source/LibraryCards.swift
+++ MagicApp/Source/LibraryCards.swift
@@ -8,28 +8,38 @@
 import Foundation
 import UIKit
 
-class libraryCards: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource{
+class libraryCards: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UITextFieldDelegate{
     
     @IBOutlet weak var cardsView: UICollectionView!
-    @IBOutlet weak var backgroundPattern: UIImageView!
+    @IBOutlet weak var txtName: UITextField!
     var library = Cards()
     var response = HTTPURLResponse()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        cardsView.backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.0)
+        
+        //Request
         getCards()
+        
+        //Collection delegation
         cardsView.delegate = self
         cardsView.dataSource = self
+        
+        //Visual
         cardsView.backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.0)
+        
+        //Functionalities
+        txtName.delegate = self
         
         
     }
     
+    // MARK: Status bar
     override var preferredStatusBarStyle: UIStatusBarStyle{
         return .lightContent
     }
     
+    // MARK: Collection View
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return library.cards?.count ?? 0
     }
@@ -52,6 +62,7 @@ class libraryCards: UIViewController, UICollectionViewDelegate, UICollectionView
     }
     
     
+    // MARK: API REQUESTS
     func getCards(){
         var request = URLRequest(url: URL(string: "https://api.magicthegathering.io/v1/cards")!)
         request.httpMethod = "GET"
@@ -71,6 +82,40 @@ class libraryCards: UIViewController, UICollectionViewDelegate, UICollectionView
             }
         }.resume()
     }
+    
+    func getCardByName(name: String){
+        var request = URLRequest(url: URL(string: "https://api.magicthegathering.io/v1/cards?name=\(name)")!)
+        request.httpMethod = "GET"
+        
+        URLSession.shared.dataTask(with: request) {(data, response, error) in
+            
+        guard let data = data else { return }
+        
+        do {
+            self.library = try JSONDecoder().decode(Cards.self, from: data)
+            self.response = response as! HTTPURLResponse
+            DispatchQueue.main.async {
+                self.cardsView.reloadData()
+            }
+            
+        } catch let error as NSError {
+            print("Failed to load: \(error.localizedDescription)")
+        }
+    }.resume()
+    }
+    
+    // MARK: Keyboard related
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        getCardByName(name: txtName.text!)
+        return false
+    }
+    
+    @IBAction func bttLimpar(_ sender: Any) {
+        txtName.text = ""
+    }
+    
     
 }
 
